6b6417826223a062c05a8d733a0127aa241b2c8f
OllieYu <Ollieyyq@gmail.com>
2022-07-06 05:43:46 +1200

    Add React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import CoffeeShop from './components/coffeeShop'
+// import Animations from '@/assets/utils/animations';
+
+function App() {
+  return (
+    <Fragment>
+      <CoffeeShop />
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/components/coffeeShop.js
@@ -0,0 +1,216 @@
+import { useRef, useEffect, useState, Fragment } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import coffeeShopModel from '../assets/CoffeeShop.glb'
+import loadGLTFModel from './loadGLTFModel'
+import gsap from 'gsap'
+
+const CoffeeShop = () => {
+    const canvasRef = useRef()
+    const [loading, setLoading] = useState(true)
+    const [progress, setProgress] = useState(0)
+    const [renderer, setRenderer] = useState()
+  
+  
+    useEffect(() => {
+      const { current: container } = canvasRef
+      if (container && !renderer) {
+        const scW = container.clientWidth
+        const scH = container.clientHeight
+        const renderer = new THREE.WebGLRenderer({
+          antialias: true,
+          alpha: true
+        })
+        renderer.setPixelRatio(window.devicePixelRatio)
+        renderer.setSize(scW, scH)
+        renderer.outputEncoding = THREE.sRGBEncoding
+        container.appendChild(renderer.domElement)
+        setRenderer(renderer)
+  
+        const scene = new THREE.Scene()
+        //add Perspective Camera
+        const camera = new THREE.PerspectiveCamera(
+          30,
+          window.innerWidth / window.innerHeight
+        )
+        camera.position.set(-31, 34, 50)
+        // const initialCameraPosition = new THREE.Vector3(
+        //   20 * Math.sin(0.2 * Math.PI),
+        //   10,
+        //   20 * Math.cos(0.2 * Math.PI)
+        // );
+
+        //add ambient Light
+        const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
+        scene.add(ambientLight)
+        
+        //add Orbit Controls
+        const controls = new OrbitControls(camera, renderer.domElement)
+        controls.target.set(0, 0.7, 0)
+        controls.update()
+        controls.enableDamping = false
+        controls.enablePan = true
+        
+        //load model
+        const manager = new THREE.LoadingManager()
+        loadGLTFModel(
+          scene,
+          coffeeShopModel,
+          {
+            receiveShadow: true,
+            castShadow: true
+          },
+          manager
+        ).then(() => {
+          animate()
+        })
+        
+        //loading progress
+        manager.onLoad = function () {
+          timer = setTimeout(() => {
+            setLoading(false)
+          }, 50)
+          gsap.to(controls.object.position, {
+            x:5,
+            y:3,
+            z:5.6,
+            duration: 2,
+            ease: 'power2.out'
+            // onUpdate: () => {
+            //     camera.lookAt(controls.target)
+            // }
+        })
+        }
+  
+        manager.onProgress = async (url, itemsLoaded, itemsTotal) => {
+          setProgress((itemsLoaded / itemsTotal) * 100)
+        }
+  
+        toAbout = () => {
+            gsap.to(controls.object.position, {
+                x:0.37590085647125704,
+                y:0.3758875643100988,
+                z:0.5556856584049695,
+                duration: 1.5,
+                ease: 'power2.out',
+            })
+            gsap.to(controls.target, {
+                x:0.3222232250305656,
+                y:0.26973060768776,
+                z:0.023572226956689025,
+                duration: 1.5,
+                ease: 'power2.out'
+                // onUpdate: () => {
+                //     camera.lookAt(controls.target)
+                // }
+            })
+            
+        }
+
+        toProject = () => {
+            gsap.to(controls.object.position, {
+                x:0.22872587002151068,
+                y:0.48851509821204314,
+                z:0.009777768366045395,
+                duration: 1.5,
+                ease: 'power4.out',
+            })
+            gsap.to(controls.target, {
+                x:0.43745293657515133,
+                y:0.4517064975377385,
+                z:0.09375796482458582,
+                duration: 1.5,
+                ease: 'power4.out'
+                // onUpdate: () => {
+                //     camera.lookAt(controls.target)
+                // }
+            })
+            
+        }
+
+        let req = null
+        const animate = () => {
+          req = requestAnimationFrame(animate)
+          controls.update()
+  
+          window.onresize = function () {
+            camera.aspect = window.innerWidth / window.innerHeight
+            camera.updateProjectionMatrix()
+            renderer.setSize(window.innerWidth, window.innerHeight)
+          }
+  
+        //   window.addEventListener('click', (e) => {
+        //     console.log(controls.object.position)
+        //     console.log(controls.target)
+        //   })
+
+          renderer.render(scene, camera)
+        }
+  
+        return () => {
+          cancelAnimationFrame(req)
+          renderer.dispose()
+        }
+      }
+    }, [])
+  
+    return (
+      <div
+        style={{
+          margin: 0,
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: '#bfe3dd'
+        }}
+        ref={canvasRef}
+      >
+        {loading ? (
+          <div
+            style={{
+              height: '100vh',
+              width: '100vw',
+              position: 'fixed',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}
+          >
+            <span style={{ alignSelf: 'center' }}>Boiling the water...</span>
+            <progress
+              style={{ width: '60vw', alignSelf: 'center' }}
+              className="nes-progress is-error"
+              value={progress}
+              max="100"
+            />
+          </div>
+        ) : (
+        <div style={{position: 'fixed'}}>
+          <div className="btnGroup">
+            <button type="button" className="nes-btn is-primary">
+                Ollie Yu
+            </button>
+            <button type="button" onClick={() => toProject()} className="nes-btn is-primary">
+                Project
+            </button>
+            <button type="button" onClick={() => toAbout()} className="nes-btn is-primary">
+                About
+            </button>
+          </div>
+          <section className="icon-list">
+            <a href="mailto:ollieyyq@gmail.com">
+                <i className="nes-icon gmail is-medium nes-pointer"></i>
+            </a>
+                <a href="https://github.com/OllieYu">
+            <i className="nes-icon github is-medium nes-pointer"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/ollie-yu-7a23851b9/">
+                <i className="nes-icon linkedin is-medium nes-pointer"></i>
+            </a>
+          </section>
+        </div>
+        )}
+      </div>
+    )
+  }
+
+  export default CoffeeShop
